b5037cb71af64394d9d6a88d0daf17ef1c79178a
David Amsalem <46437319+DavidAmsalem@users.noreply.github.com>
2023-08-17 23:01:57 +0300

    Update set-route-53.sh
    Add more efficiency to the script with reduced calls to the API

--- set-route-53.sh
+++ set-route-53.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-# Extract information about the Instance
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id/)
-AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/)
-MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4/)
 
-# Extract tags associated with instance
-ZONE_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_ZONE`].Value' --output text)
-NAME_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_NAME`].Value' --output text)
+# Check if jq is installed
+if ! command -v jq &> /dev/null; then
+    echo "jq could not be found. Trying to install..."
+    sudo apt-get update -y && sudo apt-get install jq -y
+fi
+
+# Extract information about the Instance
+META_ENDPOINT="http://169.254.169.254/latest/meta-data"
+INSTANCE_ID=$(curl -s ${META_ENDPOINT}/instance-id/)
+AZ=$(curl -s ${META_ENDPOINT}/placement/availability-zone/)
+MY_IP=$(curl -s ${META_ENDPOINT}/public-ipv4/)
+
+REGION=${AZ::-1}
+
+# Extract tags associated with the instance in one call
+TAGS=$(aws ec2 describe-tags --region ${REGION} --filters "Name=resource-id,Values=${INSTANCE_ID}")
+
+ZONE_TAG=$(echo "$TAGS" | jq -r '.Tags[] | select(.Key=="AUTO_DNS_ZONE") | .Value')
+NAME_TAG=$(echo "$TAGS" | jq -r '.Tags[] | select(.Key=="AUTO_DNS_NAME") | .Value')
+
+# Check if tags are present
+if [[ -z "$ZONE_TAG" || -z "$NAME_TAG" ]]; then
+    echo "Error: Unable to fetch required tags."
+    exit 1
+fi
 
 # Update Route 53 Record Set based on the Name tag to the current Public IP address of the Instance
-aws route53 change-resource-record-sets --hosted-zone-id $ZONE_TAG --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$NAME_TAG'","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$MY_IP'"}]}}]}'
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_TAG --change-batch "{\"Changes\":[{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"$NAME_TAG\",\"Type\":\"A\",\"TTL\":300,\"ResourceRecords\":[{\"Value\":\"$MY_IP\"}]}}]}"
